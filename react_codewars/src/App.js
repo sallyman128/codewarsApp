f0e5192977e2b9989da7cd64ceb9c7f2189a667a
Salmaan Ali <salmaan.arif.ali@gmail.com>
2022-06-26 16:05:27 -0500

    setup codewars fetch

--- react_codewars/src/App.js
+++ react_codewars/src/App.js
@@ -1,7 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react'
 
-function App() {
+import { fetchCodewars } from './api/fetchRequests';
+
+const App = () => {
+
+  fetchCodewars()
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -19,7 +25,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ react_codewars/src/api/fetchRequests.js
@@ -0,0 +1,8 @@
+const fetchCodewars = async () => {
+    const url = "https://www.codewars.com/api/v1/users/sallyman128/";
+    const resp = await fetch(url)
+    const data = await resp.json()
+    console.log(data)
+}
+
+export {fetchCodewars}
