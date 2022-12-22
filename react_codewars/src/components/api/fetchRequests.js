01f14bcfbabc603436556fb5d11a59a47b5a5108
sallyman128 <salmaan.arif.ali@gmail.com>
2022-12-22 15:26:52 -0500

    restructured app

--- react_codewars/src/components/api/fetchRequests.js
+++
@@ -1,8 +0,0 @@
-const fetchCodewars = async () => {
-    const url = "https://www.codewars.com/api/v1/users/sallyman128/";
-    const resp = await fetch(url);
-    const data = await resp.json();
-    return data
-}
-
-export {fetchCodewars}
