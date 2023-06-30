d35257611489aa871a59455f0d9903194f9ad18a
sallyman128 <salmaan.arif.ali@gmail.com>
2023-06-30 16:58:01 -0400

    fix fetch request

--- src/components/api/fetchRequests.js
+++ src/components/api/fetchRequests.js
@@ -1,8 +1,8 @@
-const fetchCodewars = async (username) => {
+const fetchCodewarsData = async (username) => {
     const url = `https://www.codewars.com/api/v1/users/${username}/`;
     const resp = await fetch(url);
     const data = await resp.json();
     return data
 }
 
-export {fetchCodewars}
+export {fetchCodewarsData}
